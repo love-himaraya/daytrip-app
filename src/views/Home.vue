81622329e9f9563f1d7b3baa22d498e425c9f6f0
yuki---7777 <yukixxx7777@gmail.com>
2021-08-25 16:43:49 +0900

    directions　fetchできない

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div v-bind:class="totalTime">{{ totalTime }}</div>
+    <button v-on:click="destinationPoint">aaa</button>
   </div>
 </template>
-
+<script src="https://maps.googleapis.com/maps/api/directions/json?origin=東京駅&destination=横浜駅&mode=bus&key=AIzaSyCEzByJnyrHErn7CSPrwoh8ToTzPpKyyD0" />
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue"
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
   },
-};
+  data() {
+    return { totalTime: "" }
+  },
+  methods: {
+    destinationPoint: function () {
+      fetch(
+        "https://maps.googleapis.com/maps/api/directions/json?origin=東京駅&destination=横浜駅&key=AIzaSyCEzByJnyrHErn7CSPrwoh8ToTzPpKyyD0"
+      ).then((data) => {
+        this.totalTime = data.routes.legs.distance.text
+      })
+    },
+  },
+}
 </script>
