32ea8c55e7e3e99dad5a5e38e926f266d0ea140f
yuki---7777 <yukixxx7777@gmail.com>
2021-08-31 17:59:35 +0900

    selectpageを複数作ってStartpageで選択したものに応じたページに遷移したかったけどできない

---
+++ src/views/SelectItari.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="select">
+    <button @click="answer('小籠包')">小籠包</button>
+    <button @click="answer('麻婆豆腐')">麻婆豆腐</button>
+    <button @click="answer('チンジャオロース')">チンジャオロース</button>
+    <button @click="answer('北京ダック')">北京ダック</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    answer(select) {
+      this.$emit("set-choice", "end", select)
+      this.$router.push("/end")
+    },
+  },
+}
+</script>
+
+<style scoped></style>
