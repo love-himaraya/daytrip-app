e728b85dc0f2d622551685a4afdbea0ee1b2d242
junseinagao <junsei.nagao@gmail.com>
2021-08-31 13:24:18 +0900

    fetch関数を修正

--- src/components/rakuten_category-api.js
+++ src/components/rakuten_category-api.js
@@ -1,25 +1,37 @@
-const timeToCook = fetch(
+export const timeToCook = fetch(
   "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1099759103456456867&categoryId=10"
 )
   .then((res) => {
     return res.json()
   })
   .then((data) => {
+    let recipeIndication = []
     for (let i = 0; i < data.result.length; i++) {
-      let recipeIndication = data.result[i].recipeIndication
+      recipeIndication.push(data.result[i].recipeIndication)
     }
+    return recipeIndication
   })
 
-const dishName = function () {
+export const dishName = function () {
   fetch(
     "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1099759103456456867&categoryId=10"
   )
     .then((rea) => {
       return rea.json()
     })
-    .then((datas) => {
-      for (let i = 0; i < datas.result.length; i++) {
-        let dishDescription = datas.result[i].recipeDescription
+    .then((data) => {
+      let dishDescription = []
+      for (let i = 0; i < data.result.length; i++) {
+        dishDescription.push(data.result[i].recipeDescription)
       }
+      return dishDescription
     })
 }
+
+// Vue ファイルで読み込む時は、
+// import { timeToCook,dishName } from '@/components/rakuten_category-api.js'
+// として読み込んで
+// timeToCook().then((recipeIndication) => {
+//  console.log(recipeIndication)
+// }))
+// という風に使える。
